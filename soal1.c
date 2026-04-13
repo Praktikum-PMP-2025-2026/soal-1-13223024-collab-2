3f6782b9fb59075e59e8b37fb90097aa96184e5c
13223024-collab <13223024@std.stei.itb.ac.id>
2026-04-13 08:47:28 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,41 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 01 - Overview of C language
+ *   Hari dan Tanggal    : Senin, 13 April 2026
+ *   Nama (NIM)          : Muhammad 'Afif Yusuf (13223024)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Sebuah gerbang keamanan memproses data satu peneliti. Tentukan status akhir peneliti berdasarkan 
+ *                         level izin, suhu tubuh, kadar radiasi, dan jam kedatangan.
+ * 
+ */
+
 #include <stdio.h>
 
 int main(void) {
+    // Deklarasi Variabel//
+    int izin, suhu, radiasi, jam;
+    // Blok Input//
+    printf("input nilai izin, suhu, radiasi, jam: ");
+    scanf("%d %d %d %d", &izin, &suhu, &radiasi, &jam);
+    //Blok Proses//
+    if (radiasi >= 6) {
+        printf("TOLAK");} 
+        else if (suhu >= 390) {
+        printf("KARANTINA");}
+        else if (izin == 1 && jam < 6 || jam > 20) {
+        printf("TOLAK");}
+        else if (izin == 1) {
+        printf("MASUK");}
+        else if (izin == 2 && radiasi <= 2 && 8 < jam < 18) {
+        printf("MASUK");}
+        else if (izin == 2){
+        printf("PEMERIKSAAN");}
+        else if (izin == 3 && radiasi == 0 && suhu < 380) {
+        printf("MASUK");}
+    else {
+        printf("TOLAK");
+    }
+        
     return 0;
 }
+ 
+ 
